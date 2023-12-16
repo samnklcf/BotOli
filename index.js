c112231ed4e0ccea1eadbfc88b83c9517fdb1d48
samnklcf <samyeyebe@gmail.com>
2023-12-16 01:55:25 +0100

    samuel

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express()
 app.use(express.static('public'));
-app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
+app.use('/images', express.static('public/images'));
 
 // Ajouter des documents pour saluer
 manager.addDocument('fr', 'Bonjour', 'salutation');
